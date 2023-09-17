9c49539419b5631bf2b17431c0eaf52cafdcb82d
Paz-Santangelo <paz_santangelo@hotmail.com>
2023-09-17 16:32:09 -0300

    Subiendo proyecto vacio para comenzar a subir las clases

---
+++ SistemaGestionVentasCompras-C#/BaseDatos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+public class ClienteRepository
+{
+    private string connectionString;
+
+    public ClienteRepository(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public bool InsertarCliente(string nombre, string correo)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Clientes (Nombre, Correo) VALUES (@Nombre, @Correo)";
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Nombre", nombre);
+                command.Parameters.AddWithValue("@Correo", correo);
+
+                int rowsAffected = command.ExecuteNonQuery();
+                return rowsAffected > 0;
+            }
+        }
+    }
+}
