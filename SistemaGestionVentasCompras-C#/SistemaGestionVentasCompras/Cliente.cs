0e705fe0f14f07a8950db2c6da69e21577a5e2ae
Paz-Santangelo <paz_santangelo@hotmail.com>
2023-09-17 16:42:49 -0300

    subiendo mis clases Cliente - Producto - Venta - SistemaVentaService

---
+++ SistemaGestionVentasCompras-C#/SistemaGestionVentasCompras/Cliente.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace SistemaGestionVentasCompras
+{
+   public  class Cliente
+    {
+        public int Dni_cliente { get; set; }
+        public string NombreCompleto { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+
+        public Cliente()
+        {
+        }
+
+        public Cliente(int dni_cliente, string nombreCompleto, string telefono, string email)
+        {
+            Dni_cliente = dni_cliente;
+            NombreCompleto = nombreCompleto;
+            Telefono = telefono;
+            Email = email;
+        }
+
+        private static void ValidarDatosCliente(int dni, string nombreCompleto, string telefono, string email)
+        {
+            if (dni <= 0)
+            {
+                throw new ArgumentException("El DNI debe ser un número positivo.");
+            }
+            if (string.IsNullOrEmpty(nombreCompleto))
+            {
+                throw new ArgumentException("Debe indicar el nombre completo del cliente.");
+            }
+            if (string.IsNullOrEmpty(telefono))
+            {
+                throw new ArgumentException("Debe indicar el teléfono del cliente.");
+            }
+            if (string.IsNullOrEmpty(email))
+            {
+                throw new ArgumentException("Debe indicar el email del cliente.");
+            }
+        }
+
+        public static Cliente CrearClienteNuevo()
+        {
+            try
+            {
+                Console.WriteLine("\nIngrese los siguientes datos del cliente:");
+                Console.Write("DNI: ");
+                int dni = int.Parse(Console.ReadLine());
+                Console.Write("Nombre y Apellido: ");
+                string nombre = Console.ReadLine();
+                Console.Write("Teléfono: ");
+                string tel = Console.ReadLine();
+                Console.Write("Email: ");
+                string correo = Console.ReadLine();
+
+                ValidarDatosCliente(dni, nombre, tel, correo);
+                Cliente cliente = new Cliente(dni, nombre, tel, correo);
+
+                Console.WriteLine("\n¡¡¡CLIENTE GUARDADO CON ÉXITO!!!");
+                return cliente;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nCliente{" + "dni_cliente = " + Dni_cliente + ", nombreCompleto = " + NombreCompleto
+                + ", telefono = " + Telefono + ", email = " + Email + '}';
+        }
+    }
+}
+
---
+++ SistemaGestionVentasCompras-C#/SistemaGestionVentasCompras/SistemaVentaService.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SistemaGestionVentasCompras
+{
+    class SistemaVentaService
+    {
+        Venta venta;
+        List<Venta> listaVentas;
+
+        public SistemaVentaService()
+        {
+            this.venta = new Venta();
+            this.listaVentas = new List<Venta>();
+        }
+
+        public void MenuGestionVentas()
+        {
+            string salir = "N";
+            int opcion;
+            do
+            {
+                Console.WriteLine("\n************** BIENVENIDO A LA GESTIÓN DE VENTAS **************");
+                Console.WriteLine("¿Qué es lo que desea hacer?:");
+                Console.WriteLine("1) Ingresar una venta nueva.");
+                Console.WriteLine("2) Ver listado de todas las ventas.");
+                Console.WriteLine("3) Buscar una venta por su código.");
+                Console.WriteLine("4) Volver al menú principal.");
+                Console.Write("Ingrese la opción seleccionada: ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Venta ventaNueva = IngresoVenta();
+                        listaVentas.Add(ventaNueva);
+                        break;
+                    case 2:
+                        ListarTodasLasVentas(listaVentas);
+                        break;
+                    case 3:
+                        Console.Write("\nIngrese el código de la venta a buscar: ");
+                        int codigoBusqueda = Convert.ToInt32(Console.ReadLine());
+                        Venta ventaEncontrada = BuscarVentaPorCodigo(listaVentas, codigoBusqueda);
+                        if (ventaEncontrada != null)
+                        {
+                            Console.WriteLine("\nVenta encontrada: " + ventaEncontrada);
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nNo se encontró ninguna venta con ese código.");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("\n¿Desea volver al menú principal? (S/N): ");
+                        salir = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("\nOpción no válida.");
+                        break;
+                }
+
+            } while (salir.Equals("N", StringComparison.OrdinalIgnoreCase));
+            if (salir.Equals("S", StringComparison.OrdinalIgnoreCase))
+            {
+                //SistemaGestionVentasCompras.Main(new string[] { });
+            }
+        }
+
+        public Venta IngresoVenta()
+        {
+            Cliente cliente = new Cliente();
+            Producto producto = new Producto();
+            Cliente clienteNuevo = Cliente.CrearClienteNuevo();
+            string continuarIngreso;
+
+            Venta ventaNueva = new Venta();
+            List<Producto> carritoProductos = new List<Producto>();
+
+            do
+            {
+                Producto productoNuevo = Producto.IngresarProductoNuevo();
+                carritoProductos.Add(productoNuevo);
+
+                Console.Write("\nDesea agregar otro producto al carrito? (S/N): ");
+                continuarIngreso = Console.ReadLine();
+
+            } while (continuarIngreso.Equals("S", StringComparison.OrdinalIgnoreCase));
+
+            ventaNueva.IngresarVentaNueva(clienteNuevo, carritoProductos);
+
+            return ventaNueva;
+        }
+
+        public void ListarTodasLasVentas(List<Venta> listaVentas)
+        {
+            if (listaVentas.Count == 0)
+            {
+                Console.WriteLine("\nNo hay ventas registradas.");
+                return;
+            }
+
+            Console.WriteLine("\nLISTADO DE VENTAS:");
+            foreach (Venta venta in listaVentas)
+            {
+                Console.WriteLine(venta);
+            }
+        }
+
+        public Venta BuscarVentaPorCodigo(List<Venta> listaVentas, int codigo)
+        {
+            foreach (Venta venta in listaVentas)
+            {
+                if (venta.Codigo_venta == codigo)
+                {
+                    return venta;
+                }
+            }
+            return null;
+        }
+    }
+}
